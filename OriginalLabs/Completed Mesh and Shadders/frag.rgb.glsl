75d148c91837019415f4b084dd7ec0be07282c03
Bolanos <eduard.bolanos642@610-11412-MAC.local>
2019-10-14 10:47:37 -0700

    adding lab descriptions

---
+++ OriginalLabs/Completed Mesh and Shadders/frag.rgb.glsl
@@ -0,0 +1,20 @@
+precision mediump float;
+
+// unique to each fragment (interpolated)
+varying vec3 fragPosition;
+varying vec3 fragColor;
+varying vec3 fragNormal;
+
+// basic ambient light
+uniform vec3 ambientLight;
+
+// basic directional light
+uniform vec3 lightDirection;
+uniform vec3 lightIntensity;
+
+void main()
+{
+	vec3 light = ambientLight + lightIntensity * max(-dot(fragNormal, normalize(lightDirection)), 0.0);
+
+	gl_FragColor = vec4(light * fragColor, 1.0);
+}
---
+++ OriginalLabs/Completed uvMesh and Shadders/README.md
@@ -0,0 +1,3 @@
+# Completed Shadders
+
+1. If you cannot impement the shadders, you can use these. 
